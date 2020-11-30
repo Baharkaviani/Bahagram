b75b0032a656852ec78b6e7a1e361e035b913de3
Bahar <bhr.kaviani@gmail.com>
2020-11-30 18:53:11 +0330

    Add: user file

--- server/userManaging.py
+++ server/userManaging.py
@@ -1,23 +1,42 @@
 """
-Created on Sun Nov 29 17:43:00 2020 (1399/9/10)
+Created on Mon Nov 30 17:43:00 2020 (1399/9/10)
 @author: Bahar Kaviani
 """
 import socket
 import threading
+import user as user
 
 # global variables
 MESSAGE_LENGTH_SIZE = 64
 ENCODING = 'utf-8'
+onlineUsers = set()
+
+"""
+Defined on Mon Nov 30 18:26:00 2020 (1399/9/10)
+@author: Bahar Kaviani
+description:
+end: no
+"""
+def register(username):
+
 
 """
 Defined on Sun Nov 29 01:14:00 2020 (1399/9/9)
 @author: Bahar Kaviani
 description: set 'connected' flag to 'True' till the client sends "DISCONNECT"
              while the flag is set reads client messages from conn
+end: no
 """
 def handle_client(conn, addr):
     print("[NEW CONNECTION] Client connected from {}".format(addr))
     connected = True
+
+    # print all online users for the new connected user
+    for o in onlineUsers:
+        print(o)
+
+    # add connected user to the onlineUsers set
+    onlineUsers.add()
 
     # recieve the message from Client
     while connected:
