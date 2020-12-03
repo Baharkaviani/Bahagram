1d8f81912fa3878caa164b4e09b7d4a2ee17bb14
Bahar <bhr.kaviani@gmail.com>
2020-12-03 23:04:03 +0330

    Add: User Class

---
+++ server/user.py
@@ -0,0 +1,10 @@
+"""
+Defined on Thu Dec 3 22:58:00 2020 (1399/9/13)
+@author: Bahar Kaviani
+description: Define User class and it's functions.
+"""
+
+class User:
+    def __init__(self, IP, username):
+        self.IP = IP
+        self.username = username
